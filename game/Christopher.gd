c0eb5a7bbf44502de78d0e2c6f349b1dd3776c9a
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-11-02 16:46:29 -0700

    Created the game
    Created a basic game.
    
    CHRISTOPHER MOVES!!!!

---
+++ game/Christopher.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+var speed = 200
+var velocity = Vector2()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("up"):
+		velocity.y -= speed
+	if Input.is_action_pressed("left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("down"):
+		velocity.y += speed
+	if Input.is_action_pressed("right"):
+		velocity.x += speed
+	
+	move_and_slide(velocity)
+	look_at(get_global_mouse_position())
