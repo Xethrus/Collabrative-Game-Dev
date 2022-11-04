767b88dd64e0f62cc8301e0ca547f69dbad31886
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-11-04 12:33:30 -0700

    Organizational changes, hitbox change
    Changed slime hitbox
    
    Put all resources into folders so there isn't a mess of folders

--- game2/christopher/christopher.gd
+++ game2/christopher/christopher.gd
@@ -7,7 +7,7 @@ extends KinematicBody2D
 
 var speed = 200
 var velocity = Vector2()
-
+var health = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
